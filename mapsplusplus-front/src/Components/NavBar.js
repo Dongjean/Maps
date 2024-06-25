c333390c2040e8f70f64c6abb01fc0cb9ba600f0
Dongjin <shindongjin2004@gmail.com>
2024-06-25 16:50:34 +0800

    Added CSS for Button Hovering and Clicking for NavBar Logo

--- mapsplusplus-front/src/Components/NavBar.js
+++ mapsplusplus-front/src/Components/NavBar.js
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+
 import '../CSS/NavBarStyles.css'
 
 function NavBar() {
+    const [LogoColour, setLogoColour] = useState('transparent');
+
     return (
         <div class='NavBarContainer'>
+            <div
+                class='Logo'
 
-            <div class='Logo'>
+                onMouseEnter={() => {setLogoColour('darkgrey')}}
+                onMouseLeave={() => {setLogoColour('transparent')}}
+
+                onMouseDown={() => {setLogoColour('grey')}}
+                onMouseUp={() => {setLogoColour('darkgrey')}}
+
+                style={{backgroundColor: LogoColour, cursor: 'pointer'}}
+            >
                 <div class='LogoPlaceholderText'>
                     Maps++
                 </div>
