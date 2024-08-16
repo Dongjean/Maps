878722a26386fc82624228b78b88fb6a4b63066e
Dongjin <shindongjin2004@gmail.com>
2024-08-16 12:04:51 +0800

    Added Backend Java Files with Simple Getters for Maps Travel Route data

---
+++ mapsplusplus-back/src/test/java/com/wgdap/mapsplusplus/MapsplusplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wgdap.mapsplusplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapsplusplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
